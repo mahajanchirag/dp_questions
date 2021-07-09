cfa35afcd1dd0a5e7c0586808eb68668705d170e
mahajanchirag <70029905+mahajanchirag@users.noreply.github.com>
2021-07-09 23:36:34 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Longest Palindromic Subsequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    
+    
+    
+    int longestPalindromeSubseq(string s) {
+        
+        string b=s;
+        reverse(b.begin(),b.end());
+        
+        int n=s.length();
+        
+        int dp[n+1][n+1];
+        
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 ||j==0)
+                {
+                    dp[i][j]=0;
+                }
+                
+                else if(s[i-1]==b[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+                
+            }
+        }
+        
+        return dp[n][n];
+    }
+};
